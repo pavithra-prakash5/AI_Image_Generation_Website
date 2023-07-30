6f5c4f5e5095bc3bb6f737529345e50087c0e0cf
pavithraprakash1 <pavithraprakash092@gmail.com>
2023-07-30 22:34:19 +0530

    first commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://localhost:5173/api/v1/post', {
+      const response = await fetch('https://ai-image-generation-xgsy.onrender.com//api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
